625d9928e048cc5eafa085896af48545b6d808dd
bhupeshbhatia <bhupesh.bhatia@gmail.com>
2020-08-20 00:20:51 -0400

    Working sudoku solver. Requires improvement

---
+++ main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import sudoku_solver
+import numpy as np
+
+board = Path('puzzles/simple-1.txt').read_text()
+
+f = open('puzzles/hardsudoku-3.txt', 'r')
+lines = f.read()
+f.close()
+sudoku = [[character for character in line if not character==" "] for line in lines.split("\n")]
+
+def change_to_zero(sudoku):
+  
+  for r in range(9):
+    for c in range(9):
+      if sudoku[r][c] == "_":
+        sudoku[r][c] = '0'
+
+    sudoku[r] = list(map(int, sudoku[r]))
+  return sudoku
+
+
+result = np.array(change_to_zero(sudoku))
+print(result)
+sudoku_solver.solve_sudoku(result)
+print(result)
+print(sudoku_solver.is_board_solved(result))
